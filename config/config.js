c60ade014b22132979d8d17d8d56e76ddd30586a
Michael Gale <michael.gale@outlook.com.au>
2019-04-29 17:55:42 +1000

    update

--- config/config.js
+++ config/config.js
@@ -1,2 +1,4 @@
 var mysql = require('mysql');
-module.exports = connection = mysql.createConnection(process.env.CLEARDB_DATABASE_URL);
+module.exports = connection = mysql.createConnection(
+  process.env.CLEARDB_DATABASE_URL
+);
